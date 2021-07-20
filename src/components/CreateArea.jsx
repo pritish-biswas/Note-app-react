1410ccbfb8f7e34f7979434af3eaa6f138d90576
Pritish Biswas <pritishbiswas02@gmail.com>
2021-07-20 12:32:56 +0530

    ready for deployment phase 1

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [isClick, setIsClick] = useState(false);
+
+  function expand() {
+    setIsClick(true);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isClick && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isClick ? "3" : "1"}
+        />
+        {isClick && (
+          <Zoom in={true}>
+            <Fab onClick={submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <MenuBookIcon />
+         Keeps
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
